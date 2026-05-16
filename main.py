c0a1aab951b7cbac910470cdd53531018098f460
Chunninck <arbuzarbuz234@gmail.com>
2026-05-16 10:40:27 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from pygame import *
 
 #шрифты и надписи
 font.init()
-font1 = font.SysFont('Arial', 80)
+font1 = font.Font(None, 80)
 lose_1 = font1.render('Игрок 1 проиграл', True, (255, 255, 255))
+lose_2 = font1.render('Игрок 2 проиграл', True, (255, 255, 255))
 
 #нам нужны такие картинки:
 img_platform = "platform.png" #платформа
-img_ball = "ball.png"
-img_back = "background.png"
+img_ball = "ball.png" #мяч
+img_back = "background.png" #фон
 
 #класс-родитель для других спрайтов
 class GameSprite(sprite.Sprite):
@@ -28,45 +29,90 @@ class GameSprite(sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
- #метод, отрисовывающий героя на окне
+    #метод, отрисовывающий героя на окне
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 #класс главного игрока
 class Player(GameSprite):
-   #метод для управления спрайтом стрелками клавиатуры
+    #метод для управления спрайтом стрелками клавиатуры
     def update_r(self):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
         if keys[K_DOWN] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
-
+    #метод для управления буквами
     def update_l(self):
         keys = key.get_pressed()
-        if keys[K_W] and self.rect.y > 5:
+        if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_S] and self.rect.y < win_height - 80:
+        if keys[K_s] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
 
-#создаем окошко
+class Ball(GameSprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, speed_y, speed_x):
+        super().__init__(player_image, player_x, player_y, size_x, size_y, 0)
+        self.speed_y = speed_y
+        self.speed_x = speed_x
+
+    def update(self): 
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+
+        if self.rect.y > win_height - 50 or self.rect.y < 0:
+            self.speed_y *= -1
+
+
+
+#окно
 win_width = 1600
 win_height = 900
 display.set_caption("PingPong")
 window = display.set_mode((win_width, win_height))
+#background = transform.scale(image.load(img_back), (win_width, win_height))
+background = Surface((win_width, win_height))
+background.fill((255, 255, 255))
 
+#объекты
+platform_2= Player(img_platform, 150, 300, 30, 300, 10)
+platform_1= Player(img_platform, 1450, 300, 30, 300, 10)
+ball = Ball(img_ball, 500, 300, 50, 50, 7, 7)
 
-platform_1 = Player(img_platform, 150, 300, 30, 300, 10)
-platform_2 = Player(img_platform, 1450, 300, 30, 300, 10)
-
-#основной цикл игры:
-run = True #флаг сбрасывается кнопкой закрытия окна
+#игровой цикл
+finish = False
+run = True
 while run:
     #событие нажатия на кнопку Закрыть
     for e in event.get():
         if e.type == QUIT:
             run = False
+        if not finish:
+            window.blit(background, (0,0))
+            platform_2.update_l()
+            platform_2.reset()
+        
+            platform_1.update_r()
+            platform_1.reset()
+        
+            ball.reset()
+            ball.update()
+        
+            if sprite.collide_rect(platform_1, ball) or sprite.collide_rect(platform_2, ball):
+                ball.speed_x *= -1
+        
 
+            if ball.rect.x <0:
+                
+                window.blit(lose_1,(600, 400))
+                display.update()
+                finish = True
+
+
+            if ball.rect.x > win_width-50:  
+                
+                window.blit(lose_2,(600, 400))
+                display.update()
+                finish = True
 
         display.update()
-    #цикл срабатывает каждую 0.05 секунд
-        time.delay(50)
+        time.delay(60)
